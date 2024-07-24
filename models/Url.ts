ef8e96a672979a310157572428a82f96817d8503
aliabdo6 <aliellool202020@gmail.com>
2024-07-24 04:01:03 +0300

    Initial commit from Create Next App

---
+++ models/Url.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UrlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Url ||
+  mongoose.model("Url", UrlSchema);
